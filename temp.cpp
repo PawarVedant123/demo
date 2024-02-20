bb7d6dbf20ad6238c1cc789328992a7994808021
PawarVedant123 <vedantp601@gmail.com>
2024-02-20 09:43:48 +0530

    yay new change added

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main()
-{
-    cout<<"Hello world";
+
+int main() {
+    const int n = 5; // specify the number of arrays
+
+    // Declare a 2-dimensional vector of empty vectors
+    vector<vector<int>> arr(n);
+
+    // Accessing an element
+    // For example, setting the element at index 2 to [1, 2, 3]
+    arr[2] = {1, 2, 3};
+
+    // Displaying the arrays
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < arr[i].size(); ++j) {
+            cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
